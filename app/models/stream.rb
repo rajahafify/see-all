5946aeb086268faf1f238e1bafd6372a42aa2f30
Raja Hafify <rajahafify.zaed@gmail.com>
2024-09-15 22:16:11 +0800

    Add spec for stream

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,3 +1,4 @@
 class Stream < ApplicationRecord
   has_one_attached :recording
+  validates :stream_key, presence: true
 end
--- spec/models/stream_spec.rb
+++ spec/models/stream_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Stream, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    stream = Stream.new(stream_key: 'unique_key_123') # Changed to use stream_key
+    expect(stream).to be_valid
+  end
+
+  it 'is not valid without a stream_key' do
+    stream = Stream.new(stream_key: nil) # Only checking stream_key
+    expect(stream).to_not be_valid
+  end
+
+  # Add more tests as needed
 end
