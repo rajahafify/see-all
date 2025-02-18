04add13071aaede27b36b4f59a1ef06de49e9140
Raja Hafify <rajahafify.zaed@gmail.com>
2025-02-18 22:03:52 +0800

    Add Twilio integration for SMS notifications
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/rajahafify/see-all?shareId=XXXX-XXXX-XXXX-XXXX).

---
+++ config/initializers/twilio.rb
@@ -0,0 +1,6 @@
+Twilio.configure do |config|
+  config.account_sid = ENV['TWILIO_ACCOUNT_SID']
+  config.auth_token = ENV['TWILIO_AUTH_TOKEN']
+end
+
+TWILIO_PHONE_NUMBER = ENV['TWILIO_PHONE_NUMBER']
